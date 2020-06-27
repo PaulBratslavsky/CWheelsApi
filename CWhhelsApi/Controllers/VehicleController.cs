881fb73398be665f176a084cb93598a8e4f41c7e
paulbratslavsky <paul.bratslavsky@gmail.com>
2020-06-27 02:38:46 -0700

    added Get Post Put Delete methods to wheels contronller

---
+++ CWhhelsApi/Controllers/VehicleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CWhhelsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CWhhelsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private static List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Vehicle() {Id = 0, Title = "My Car", Price = 300},
+            new Vehicle() {Id = 1, Title = "Other Car", Price = 3100},
+        };
+
+        [HttpGet]
+        public IEnumerable<Vehicle> Get()
+        {
+            return vehicles;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Vehicle vehicle)
+        {
+            vehicles[id] = vehicle;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            vehicles.RemoveAt(id);
+        }
+    }
+}
