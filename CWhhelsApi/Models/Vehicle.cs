76ad77e9ca54e7e82cb0293fd276852b379e1bea
paulbratslavsky <paul.bratslavsky@gmail.com>
2020-06-27 02:05:02 -0700

    Add project files.

---
+++ CWhhelsApi/Models/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CWhhelsApi.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+}
